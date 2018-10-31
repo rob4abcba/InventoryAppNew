ecc3fc634ef6b3a36e0c057658008f7800347222
catielynncodes <caitinthewoods@gmail.com>
2018-10-31 05:01:28 -0400

    Issue: added in code for URIMatcher from given Udacity gist. Cannot replace name of PATH_PETS to PATH_ITEMS.

--- app/src/main/java/com/example/android/pets/data/ItemProvider.java
+++ app/src/main/java/com/example/android/pets/data/ItemProvider.java
@@ -2,6 +2,7 @@ package com.example.android.pets.data;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
 import android.net.Uri;
 
@@ -65,4 +66,36 @@ public class ItemProvider extends ContentProvider {
     public String getType(Uri uri) {
         return null;
     }
+
+
+
+    // Code below is the last added
+
+
+
+    /** URI matcher code for the content URI for the items table */
+    private static final int ITEMS = 100;
+
+    /** URI matcher code for the content URI for a single item in the items table */
+    private static final int ITEM_ID = 101;
+
+    /**
+     * UriMatcher object to match a content URI to a corresponding code.
+     * The input passed into the constructor represents the code to return for the root URI.
+     * It's common to use NO_MATCH as the input for this case.
+     */
+    private static final UriMatcher sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+    // Static initializer. This is run the first time anything is called from this class.
+    static {
+        // The calls to addURI() go here, for all of the content URI patterns that the provider
+        // should recognize. All paths added to the UriMatcher have a corresponding code to return
+        // when a match is found.
+
+        sUriMatcher.addURI(ItemContract.CONTENT_AUTHORITY, ItemContract.PATH_ITEMS, ITEMS);
+        sUriMatcher.addURI(ItemContract.CONTENT_AUTHORITY, ItemContract.PATH_ITEMS + "/#");
+
+
+    }
+
 }
